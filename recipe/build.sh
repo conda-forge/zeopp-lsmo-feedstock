93428931d9e69baa332c77f1c7aebbb2b31edce9
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2022-07-24 22:31:18 +0200

    docs: add docstring
    to retrigger CI

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,4 +12,5 @@ CFLAGS="$CFLAGS -I$PREFIX/include/eigen3" make
 cp network ${PREFIX}/bin/
 cd ../
 
+# run tests defined in https://github.com/lsmo-epfl/zeopp-lsmo/tree/master/tests
 cd tests && bash run_tests.sh
