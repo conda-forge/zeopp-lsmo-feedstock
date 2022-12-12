dc86a247999e1e32ffa2c1de51bb937d1754a6d0
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-12-12 14:15:58 +0100

    for now test manually here for debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,4 +13,39 @@ cp network ${PREFIX}/bin/
 cd ../
 
 # run tests defined in https://github.com/lsmo-epfl/zeopp-lsmo/tree/master/tests
-cd tests && bash run_tests.sh
+cd tests
+
+function check_changes {
+    #diff=`git diff-index --quiet HEAD --`
+    diff=$(git diff --no-index $1 $2)
+    rm $1
+    if [ -n "$diff" ]; then
+        echo "### Changes detected:"
+        echo "$diff"
+        exit 1
+    fi
+}
+
+echo "Testing network -xyz ZIF-67_opt.cif"
+network -xyz ZIF-67_opt.cif
+check_changes ZIF-67_opt.xyz ZIF-67_opt_ref.xyz
+
+echo "Testing network -ha -res -allowAdjustCoordsAndCell EDI.cssr"
+network -ha -res -allowAdjustCoordsAndCell EDI.cssr
+check_changes EDI.res EDI_ref.res
+
+echo "Testing network -ha -chan 1.5 EDI.cssr"
+network -ha -chan 1.5 EDI.cssr
+check_changes EDI.chan EDI_ref.chan
+
+echo "Testing network -ha -sa 1.2 1.2 2000 EDI.cssr"
+network -ha -sa 1.2 1.2 2000 EDI.cssr
+check_changes EDI.sa EDI_ref.sa
+
+echo "Testing network -ha -vol 1.2 1.2 50000 EDI.cssr"
+network -ha -vol 1.2 1.2 50000 EDI.cssr
+check_changes EDI.vol EDI_ref.vol
+
+echo "Testing network -ha -psd 1.2 1.2 50000 EDI.cssr"
+network -ha -psd 1.2 1.2 50000 EDI.cssr
+check_changes EDI.psd_histo EDI_ref.psd_histo
