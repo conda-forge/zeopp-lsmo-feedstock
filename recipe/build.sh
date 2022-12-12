e17b5e07c032f3b6e6765832cd77b07f00506d5c
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-12-12 17:28:23 +0100

    what is going on

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,33 +19,33 @@ function check_changes {
     sleep 1
     diff=$(git --no-pager diff --no-index $1 $2)
     rm $1
-    if [ -n "$diff" ]; then
+    if [ ! -z "$diff" ]; then
         echo "### Changes detected:"
         echo "$diff"
         exit 1
     fi
 }
 
-echo "Testing network -xyz ZIF-67_opt.cif"
+echo "### Testing network -xyz ZIF-67_opt.cif"
 network -xyz ZIF-67_opt.cif
 check_changes ZIF-67_opt.xyz ZIF-67_opt_ref.xyz
 
-echo "Testing network -ha -res -allowAdjustCoordsAndCell EDI.cssr"
+echo "### Testing network -ha -res -allowAdjustCoordsAndCell EDI.cssr"
 network -ha -res -allowAdjustCoordsAndCell EDI.cssr
 check_changes EDI.res EDI_ref.res
 
-echo "Testing network -ha -chan 1.5 EDI.cssr"
+echo "### Testing network -ha -chan 1.5 EDI.cssr"
 network -ha -chan 1.5 EDI.cssr
 check_changes EDI.chan EDI_ref.chan
 
-echo "Testing network -ha -sa 1.2 1.2 2000 EDI.cssr"
+echo "### Testing network -ha -sa 1.2 1.2 2000 EDI.cssr"
 network -ha -sa 1.2 1.2 2000 EDI.cssr
 check_changes EDI.sa EDI_ref.sa
 
-echo "Testing network -ha -vol 1.2 1.2 50000 EDI.cssr"
+echo "### Testing network -ha -vol 1.2 1.2 50000 EDI.cssr"
 network -ha -vol 1.2 1.2 50000 EDI.cssr
 check_changes EDI.vol EDI_ref.vol
 
-echo "Testing network -ha -psd 1.2 1.2 50000 EDI.cssr"
+echo "### Testing network -ha -psd 1.2 1.2 50000 EDI.cssr"
 network -ha -psd 1.2 1.2 50000 EDI.cssr
 check_changes EDI.psd_histo EDI_ref.psd_histo
