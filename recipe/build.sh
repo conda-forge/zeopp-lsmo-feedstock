ac7a5fbb550fce88993357b42c17f1cd10756583
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-12-19 18:41:49 +0100

    split tests out in test section

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,3 @@ CFLAGS="$CFLAGS -I$PREFIX/include/eigen3" make
 cp network ${PREFIX}/bin/
 cd ../
 
-# run tests defined in https://github.com/lsmo-epfl/zeopp-lsmo/tree/master/tests
-cd tests
-
-pytest test_zeopp.py
